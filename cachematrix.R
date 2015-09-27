6e3e006cec40a9ece2b3ff6c5c72c545643f9941
Sofia Tania <stania.ang@gmail.com>
2015-09-27 10:48:27 +0800

    Remove comments given as part of the skeleton

--- cachematrix.R
+++ cachematrix.R
@@ -1,6 +1,5 @@
 ## A combination of these two functions allow cached computation of matrix inverse
 
-## Write a short comment describing this function
 ## Returns a matrix with some wrapper functions
 ## (a getter and setter for the matrix itself and for the cached inverse)
 makeCacheMatrix <- function(x = matrix()) {
@@ -18,12 +17,10 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## Write a short comment describing this function
 ## Returns a matrix that is the inverse of 'x'
 cacheSolve <- function(x, ...) {
   i <- x$getinverse()
   if(!is.null(i)) {
-    #message("getting cached data")
     return(i)
   }
   data <- x$get()
